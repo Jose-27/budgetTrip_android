9b698a14e9fd4fafb40d55289d591d1b06f9b230
Joe <bdj19@hotmail.com>
2017-01-03 22:28:28 -0500

    adding an activity for the autocomplete place

--- app/src/main/java/com/example/joe/budgettrip/MainActivity.java
+++ app/src/main/java/com/example/joe/budgettrip/MainActivity.java
@@ -1,20 +1,47 @@
 package com.example.joe.budgettrip;
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.icu.util.Calendar;
+import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView mCheckin, mCheckout;
+    private static final int REQUEST_CODE_AUTOCOMPLETE = 1;
+    private TextView mCheckin, mCheckout, mDeparture, mLanding;
+    private static final String TAG = "";
 
-    private int month,day,year;
+    private int month, day, year;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +51,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mCheckin = (TextView) findViewById(R.id.checkin);
         mCheckout = (TextView) findViewById(R.id.checkout);
+        mDeparture = (TextView) findViewById(R.id.departure);
+        mLanding = (TextView) findViewById(R.id.landing);
 
         mCheckin.setOnClickListener(this);
         mCheckout.setOnClickListener(this);
-        mCheckin.setCompoundDrawablesWithIntrinsicBounds(
-                R.drawable.calendar, 0, 0, 0);
+        mDeparture.setOnClickListener(this);
+        mLanding.setOnClickListener(this);
+
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onClick(View v) {
 
+        if (v == mDeparture) {
+            openAutocompleteActivity();
+        }
 
-        if (v==mCheckin){
+        if (v == mLanding) {
+            openAutocompleteActivity();
+        }
+
+        if (v == mCheckin) {
             final Calendar c = Calendar.getInstance();
 
-            month= c.get(Calendar.MONTH);
-            day=c.get(Calendar.DAY_OF_MONTH);
-            year=c.get(Calendar.YEAR);
+            month = c.get(Calendar.MONTH);
+            day = c.get(Calendar.DAY_OF_MONTH);
+            year = c.get(Calendar.YEAR);
 
             DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
 
@@ -54,12 +91,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             datePickerDialog.show();
         }
 
-        if (v==mCheckout){
+        if (v == mCheckout) {
             final Calendar c = Calendar.getInstance();
 
-            month= c.get(Calendar.MONTH);
-            day=c.get(Calendar.DAY_OF_MONTH);
-            year=c.get(Calendar.YEAR);
+            month = c.get(Calendar.MONTH);
+            day = c.get(Calendar.DAY_OF_MONTH);
+            year = c.get(Calendar.YEAR);
 
             DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
 
@@ -73,4 +110,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             datePickerDialog.show();
         }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Check that the result was from the autocomplete widget.
+        if (requestCode == REQUEST_CODE_AUTOCOMPLETE) {
+            if (resultCode == RESULT_OK) {
+                // Get the user's selected place from the Intent.
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                Log.i(TAG, "Place Selected: " + place.getName());
+
+                // Format the place's details and display them in the TextView.
+                mDeparture.setText(place.getAddress());
+
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+                Log.e(TAG, "Error: Status = " + status.toString());
+            } else if (resultCode == RESULT_CANCELED) {
+                // Indicates that the activity closed before a selection was made. For example if
+                // the user pressed the back button.
+            }
+        }
+    }
+
+    private void openAutocompleteActivity() {
+
+        try {
+            // The autocomplete activity requires Google Play Services to be available. The intent
+            // builder checks this and throws an exception if it is not the case.
+            Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                    .build(this);
+            startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE);
+        } catch (GooglePlayServicesRepairableException e) {
+            // Indicates that Google Play Services is either not installed or not up to date. Prompt
+            // the user to correct the issue.
+            GoogleApiAvailability.getInstance().getErrorDialog(this, e.getConnectionStatusCode(),
+                    0 /* requestCode */).show();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            // Indicates that Google Play Services is not available and the problem is not easily
+            // resolvable.
+            String message = "Google Play Services is not available: " +
+                    GoogleApiAvailability.getInstance().getErrorString(e.errorCode);
+
+            Log.e(TAG, message);
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
